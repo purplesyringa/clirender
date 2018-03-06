c50ba480853a097973d353441c8c843a45d9fdfb
Ivanq <imachug@yandex.ru>
2018-03-06 20:49:30 +0300

    Add Rect

---
+++ layout/node.py
@@ -0,0 +1,10 @@
+class Node(object):
+	def __init__(self):
+		self.render_offset = (None, None)
+		self.render_boundary_left_top = (None, None)
+		self.render_boundary_right_bottom = (None, None)
+
+		self.render_children = []
+
+	def render(self, layout):
+		raise NotImplementedError
