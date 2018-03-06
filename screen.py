35224c63d0876cd77083c0511395cc373c6edfd4
Ivanq <imachug@yandex.ru>
2018-03-06 20:14:37 +0300

    Move loop to separate method

--- screen.py
+++ screen.py
@@ -10,15 +10,13 @@ class Screen(object):
 	class Resized(Exception):
 		pass
 
-	def __init__(self, handler, init=None):
-		self.terminal_size = (0, 0)
+	def __init__(self):
+		self.terminal_size = self.getTerminalSize()
 
-		if init is not None:
-			init(self)
-
+	def loop(self, handler):
 		while True:
 			cur = self.getTerminalSize()
-			if self.terminal_size != (0, 0) and self.terminal_size != cur:
+			if self.terminal_size != cur:
 				raise Screen.Resized
 			self.terminal_size = cur
 
