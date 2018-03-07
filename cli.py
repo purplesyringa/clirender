d37ef518b540443a6fbf879285ebc5724b7e614b
Ivanq <imachug@yandex.ru>
2018-03-07 21:14:41 +0300

    Use XML in example

--- cli.py
+++ cli.py
@@ -1,22 +1,14 @@
+#!/usr/bin/env python
+
+from layout import xml_parser
 from layout import Layout
-from layout.rect import Rect
-from layout.stackpanel import StackPanel
 
-layout = Layout(
-	StackPanel(
-		width="100%", height="100%",
-		vertical=False,
+root = xml_parser.fromXml("""
+	<StackPanel width="100%" height="100%" vertical="False">
+		<Rect width="20%" height="100%" bg="#232A31 | white" />
+		<Rect width="80%" height="100%" bg="black" />
+	</StackPanel>
+""")
 
-		children=[
-			Rect(
-				width="20%", height="100%",
-				bg="#232A31 | white"
-			),
-			Rect(
-				width="80%", height="100%",
-				bg="black"
-			)
-		]
-	)
-)
+layout = Layout(root)
 layout.render()
