9a05dd8f311bc127d9c929eb259d445ac1d9fe81
Ivanq <imachug@yandex.ru>
2018-03-08 12:13:57 +0300

    Guess StackPanel size

--- layout/stackpanel.py
+++ layout/stackpanel.py
@@ -11,7 +11,18 @@ class StackPanel(Rect):
 		self.children = children
 
 	def render(self, layout, dry_run=False):
-		x1, y1, x2, y2 = super(StackPanel, self).render(layout, dry_run=dry_run)
+		# Guess container size
+		if self.width is None or self.height is None:
+			width, height = self.guessContainerSize(layout)
+			if self.width is not None:
+				width = self.width
+			if self.height is not None:
+				height = self.height
+		else:
+			# If the size is given, don't do unnecessary actions
+			width, height = self.width, self.height
+
+		x1, y1, x2, y2 = super(StackPanel, self).render(layout, dry_run=dry_run, width=width, height=height)
 
 		cur_x, cur_y = x1, y1
 
@@ -28,3 +39,29 @@ class StackPanel(Rect):
 				cur_x = child_x2
 
 		return x1, y1, x2, y2
+
+	def guessContainerSize(self, layout):
+		x1, y1, _, _ = super(StackPanel, self).render(layout, dry_run=True, width=0, height=0)
+
+		cur_x, cur_y = x1, y1
+		max_width, max_height = 0, 0
+
+		for child in self.children:
+			child.render_offset = (cur_x, cur_y)
+			child.render_boundary_left_top = (0, 0)
+			child.render_boundary_right_bottom = layout.screen.terminal_size
+
+			child_x1, child_y1, child_x2, child_y2 = child.render(layout, dry_run=True)
+
+			max_width = max(max_width, child_x2 - child_x1)
+			max_height = max(max_height, child_y2 - child_y1)
+
+			if self.vertical:
+				cur_y = child_y2
+			else:
+				cur_x = child_x2
+
+		if self.vertical:
+			return max_width, cur_y - y1
+		else:
+			return cur_x - x1, max_height
