2b2d69b8c863cfa5b14daf9bc413f0f09882cf42
Ivanq <imachug@yandex.ru>
2018-03-11 12:50:37 +0300

    '=' signs are unnecessary for offset/values to be calculated as expressions

--- layout/layout.py
+++ layout/layout.py
@@ -22,50 +22,40 @@ class Layout(object):
 
 		self.root.render(self)
 
-	def calcRelativeSize(self, size, total, stretch=None):
+	def calcRelativeSize(self, size, total, stretch=None, expression=True):
+		if not expression:
+			# Maybe just absolute integer or float?
+			try:
+				return float(size)
+			except ValueError:
+				pass
+
+			# Percent?
+			try:
+				if size.endswith("%"):
+					return float(size[:-1]) / 100 * total
+			except ValueError:
+				pass
+
+			raise ValueError("Cannot parse %s as non-expression offset/size" % size)
+
+		if not isinstance(size, str) and not isinstance(size, unicode):
+			return self.calcRelativeSize(size, total, stretch=stretch, expression=False)
+
+
 		if stretch is not None:
 			stretch = self.calcRelativeSize(stretch, total)
 
-		# Maybe just absolute integer or float?
+
+		# Replace percents
+		size = re.sub(r"\b([\d\.]+%)(?=\s|$)", lambda x: str(self.calcRelativeSize(x.group(1), total, stretch=stretch, expression=False)), size)
+
 		try:
-			return float(size)
-		except ValueError:
-			pass
+			data = {}
+			if stretch is not None:
+				data["stretch"] = stretch
 
-		# If it isn't a string, what can it be?
-		if not isinstance(size, str) and not isinstance(size, unicode):
-			raise ValueError("Offset/size is %s, not int or str" % type(size))
-
-		# Maybe this is a str(int) ?
-		try:
-			return int(size)
-		except ValueError:
-			pass
-
-		# Percent?
-		try:
-			if size.endswith("%"):
-				return float(size[:-1]) / 100 * total
-		except ValueError:
-			pass
-
-		# Calculations?
-		if size.startswith("="):
-			expression = size[1:]
-
-			# Replace percents
-			expression = re.sub(r"\b([\d\.]+%)(?=\s|$)", lambda x: str(self.calcRelativeSize(x.group(1), total, stretch=stretch)), expression)
-
-			try:
-				data = {}
-				if stretch is not None:
-					data["stretch"] = stretch
-
-				return numexpr.evaluate(expression, local_dict={}, global_dict=data).item()
-			except KeyError as e:
-				if e.args[0] == "stretch":
-					raise NoStretchError()
-
-				raise
-
-		raise ValueError("Cannot parse %s as offset/size literal" % size)
+			return numexpr.evaluate(size, local_dict={}, global_dict=data).item()
+		except KeyError as e:
+			if e.args[0] == "stretch":
+				raise NoStretchError()
