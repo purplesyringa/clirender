1cc62f2b793cdb7ca3c13e5e746518ca405d1c14
Ivanq <imachug@yandex.ru>
2018-03-13 07:53:33 +0300

    Add config for pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+	name = "clirender",
+	version = "0.1",
+	description = "CLI rendering engine for Python",
+	author = "Ivanq",
+	author_email = "imachug@gmail.com",
+	url = "https://github.com/imachug/clirender",
+	keywords = ["cli", "cli-application", "rendering-2d-graphics"],
+	classifiers = []
+)
