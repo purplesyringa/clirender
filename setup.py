6658818de563a9a954f723b4d42097ecbceb4c4e
Ivanq <imachug@yandex.ru>
2018-03-13 19:10:36 +0300

    Change setup

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(
 	name = "clirender",
-	version = "0.2",
+	version = "0.3",
 	description = "CLI rendering engine for Python",
+	packages = find_packages(),
 	author = "Ivanq",
 	author_email = "imachug@gmail.com",
 	url = "https://github.com/imachug/clirender",
