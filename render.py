19100975b1f24dd55555dd4dc4f90d520c51e8ab
Ivanq <imachug@yandex.ru>
2018-03-14 15:34:17 +0300

    Add command-line render.py script

---
+++ render.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import sys
+from clirender.layout import xml_parser, Layout
+
+with open(sys.argv[1]) as f:
+	xml = f.read()
+
+root = xml_parser.fromXml(xml)
+
+layout = Layout(root)
+layout.render()
