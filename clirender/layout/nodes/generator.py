54983a89253d7bddcbebc83d0fdb57dda5a99446
Ivanq <imachug@yandex.ru>
2018-04-01 21:03:56 +0300

    Init Generators when first generated

--- clirender/layout/nodes/generator.py
+++ clirender/layout/nodes/generator.py
@@ -6,13 +6,17 @@ class Generator(object):
 		self.children = children
 		self.slots = slots
 		self._cached = None
-
-		self.init(**kwargs)
+		self._kwargs = kwargs
+		self._initted = False
 
 	def init(self):
 		pass
 
 	def generate(self):
+		if not self._initted:
+			self.init(**self._kwargs)
+			self._initted = True
+
 		return self.onGenerate()
 
 	def onGenerate(self):
