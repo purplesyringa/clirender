7126025d532e7f9981bd6b19e26723913fa3e6ee
Ivanq <imachug@yandex.ru>
2018-04-01 09:40:05 +0300

    FromXml is a factory

--- clirender/layout/nodes/fromxml.py
+++ clirender/layout/nodes/fromxml.py
@@ -5,14 +5,11 @@ class NoDefault(object):
 
 def fromXml(elem, slots, name, defines, container, additional_nodes):
 	class FromXml(Node):
-		def __init__(self, **attrs):
-			if self.text_container:
-				super(FromXml, self).__init__(value=attrs["value"])
+		def __new__(cls, **attrs):
+			if cls.text_container:
 				attrs[""] = attrs["value"]
 				del attrs["value"]
-			elif self.container:
-				super(FromXml, self).__init__(children=attrs["children"])
-
+			elif cls.container:
 				if len(attrs["children"]) > 1:
 					raise ValueError("Too many nodes in <%s>" % name)
 				elif len(attrs["children"]) == 1:
@@ -22,28 +19,22 @@ def fromXml(elem, slots, name, defines, container, additional_nodes):
 					raise ValueError("Too less nodes in <%s>" % name)
 
 
-			self.slots = dict(**slots)
+			cur_slots = dict(**slots)
 			for attr, value in attrs.items():
-				if attr not in self.slots and attr != "":
+				if attr not in cur_slots and attr != "":
 					raise ValueError("Unexpected slot :%s" % attr)
 
-				self.slots[attr] = value
+				cur_slots[attr] = value
 
-			for slot in self.slots.values():
+			for slot in cur_slots.values():
 				if isinstance(slot, NoDefault):
 					raise ValueError("Required slot :%s not passed" % slot)
 
-		def render(self, *args, **kwargs):
 			from ..xml_parser import handleElement
-			node = handleElement(elem, defines=defines, slots=self.slots, additional_nodes=additional_nodes)[0]
-
-			node.render_offset = self.render_offset
-			node.render_boundary_left_top = self.render_boundary_left_top
-			node.render_boundary_right_bottom = self.render_boundary_right_bottom
-			node.parent = self.parent
-			node.render_stretch = self.render_stretch
-
-			return node.render(*args, **kwargs)
+			items = handleElement(elem, defines=defines, slots=cur_slots, additional_nodes=additional_nodes)
+			if len(items) != 1:
+				raise ValueError("<Define> must contain exactly one node")
+			return items[0]
 
 	FromXml.text_container = container == "text"
 	FromXml.container = container == "node"
