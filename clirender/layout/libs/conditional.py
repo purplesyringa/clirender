0af6bdae253729828cfae9e9bfce601d5e54731d
Ivanq <imachug@yandex.ru>
2018-04-01 22:17:36 +0300

    Add <If with> syntax

--- clirender/layout/libs/conditional.py
+++ clirender/layout/libs/conditional.py
@@ -10,24 +10,65 @@ class Conditional(Library):
 		container = True
 		text_container = False
 
-		def init(self, is_=None):
+		def init(self, is_, with_=None, then=None, else_=None):
 			self.is_ = is_
+			self.with_ = with_
+			self.then = then
+			self.else_ = else_
+
+			if self.with_ is None:
+				if then is not None:
+					raise ValueError("Unexpected 'then' argument of <If>")
+				elif else_ is not None:
+					raise ValueError("Unexpected 'else' argument of <If>")
+			else:
+				if then is None:
+					raise ValueError("Expected 'then' argument of <If with>")
+				elif else_ is None:
+					raise ValueError("Expected 'else' argument of <If with>")
 
 		def onGenerate(self):
 			from ..xml_parser import handleElement
 
 			result = self.evaluate()
 
-			res = []
-			for child in self.children:
-				if child.tag == "Then":
-					if result:
-						res += handleElement(child, self.defines, self.slots, self.additional_nodes)
-				elif child.tag == "Else":
-					if not result:
-						res += handleElement(child, self.defines, self.slots, self.additional_nodes)
+			if self.with_ is None:
+				# <If is="condition">
+				#   <Then>
+				#     <A />
+				#   </Then>
+				#   <Else>
+				#     <B />
+				#   </Else>
+				# </If>
+
+				res = []
+				for child in self.children:
+					if child.tag == "Then":
+						if result:
+							res += handleElement(child, self.defines, self.slots, self.additional_nodes)
+					elif child.tag == "Else":
+						if not result:
+							res += handleElement(child, self.defines, self.slots, self.additional_nodes)
+					else:
+						raise ValueError("Unexpected %s inside <If>" % child)
+			else:
+				# <If with="slot" is="condition" then="A" else="B">
+				#   <Slot name="slot" />
+				# </If>
+
+				slots = dict(**self.slots)
+				if result:
+					slots[self.with_] = self.then
 				else:
-					raise ValueError("Unexpected %s inside <If>" % child)
+					slots[self.with_] = self.else_
+
+				res = []
+				for child in self.children:
+					if child.tag == ("Then", "Else"):
+						raise ValueError("Unexpected %s inside <If with>" % child)
+
+					res += handleElement(child, self.defines, slots, self.additional_nodes)
 
 			return res
 
