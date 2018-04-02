1e56e2ba5ef4e262da1d5ae84b282f5a5b32abff
Ivanq <imachug@yandex.ru>
2018-04-02 20:07:38 +0300

    v0.34

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
 from setuptools import setup, find_packages
 setup(
 	name = "clirender",
-	version = "0.33.3",
+	version = "0.34",
 	description = "CLI rendering engine for Python",
 	long_description = readme,
 	install_requires = ["colorama", "lxml", "asteval"],
