9d38ec46b2d0178ba8db48900d392defe2164a35
Ivanq <imachug@yandex.ru>
2018-04-02 12:37:50 +0300

    Fix

--- clirender/layout/libs/conditional.py
+++ clirender/layout/libs/conditional.py
@@ -1,5 +1,6 @@
 from library import Library
 from ..nodes import Generator
+from ..safe_eval import safeEval
 
 class Conditional(Library):
 	def __init__(self, layout):
@@ -78,7 +79,6 @@ class Conditional(Library):
 				scope.update(handler(self))
 
 			try:
-				from safe_eval import safeEval
 				value = safeEval(self.is_, scope)
 			except KeyError, e:
 				raise ValueError("Unknown variable used in conditional: %s" % e[0])
