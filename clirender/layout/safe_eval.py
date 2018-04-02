81104008cef9bbfcf66cf890f16efb031f8fc219
Ivanq <imachug@yandex.ru>
2018-04-02 12:39:53 +0300

    Use asteval for evaluating expressions

--- clirender/layout/safe_eval.py
+++ clirender/layout/safe_eval.py
@@ -1,4 +1,15 @@
+from asteval import Interpreter
+
 def safeEval(expr, scope):
-	scope = dict(**scope)
-	scope["__builtins__"] = None
-	return eval(expr, scope)
+	interpreter = Interpreter(
+		usersyms=dict(**scope),
+		use_numpy=False,
+		minimal=False
+	)
+
+	res = interpreter.eval(expr, show_errors=False)
+
+	if interpreter.error:
+		raise interpreter.error
+
+	return res
