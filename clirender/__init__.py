e1d30875e77c32620660c424f7781dd03052770b
Ivanq <imachug@yandex.ru>
2018-04-02 13:39:11 +0300

    Add easyRender() method

--- clirender/__init__.py
+++ clirender/__init__.py
@@ -1,2 +1,14 @@
 from screen import *
 from layout import *
+
+def easyRender(xml):
+	from layout.xml_parser import gatherLibs, fromXml
+	from layout.libs import getAdditionalNodes, render
+
+	libs = gatherLibs(xml)
+	nodes = getAdditionalNodes(libs)
+	root = fromXml(xml, additional_nodes=nodes)
+
+	layout = Layout(root)
+	render(layout, libs)
+	return layout
--- render.py
+++ render.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
 import sys
-from clirender.layout import xml_parser, Layout
-from clirender.layout.libs import render, getAdditionalNodes
+from clirender import easyRender
 
 with open(sys.argv[1]) as f:
 	xml = f.read()
 
-libs = xml_parser.gatherLibs(xml)
-nodes = getAdditionalNodes(libs)
-root = xml_parser.fromXml(xml, additional_nodes=nodes)
-
-layout = Layout(root)
-render(layout, libs)
+easyRender(xml)
