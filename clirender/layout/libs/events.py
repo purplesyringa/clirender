d7842b9d158dd46952dcae686697446a8aab3788
Ivanq <imachug@yandex.ru>
2018-04-03 21:12:33 +0300

    Handle on... handlers

--- clirender/layout/libs/events.py
+++ clirender/layout/libs/events.py
@@ -6,9 +6,21 @@ class Events(Library):
 	@patch(Node, Generator)
 	class Node(object):
 		def __init__(self, **kwargs):
-			patch.super(self).__init__(**kwargs)
-
 			self._events = {}
+
+			for name, handler in kwargs.items():
+				if name.startswith("on") and name != "on":
+					event = name[2].lower() + name[3:]
+					if not callable(handler):
+						raise ValueError("Non-callable %s handler was passed to %s: %s" % (name, self, handler))
+
+					del kwargs[name]
+
+					handler = Events.safe(handler, self)
+
+					self.on(event, handler)
+
+			patch.super(self).__init__(**kwargs)
 
 		def on(self, event, handler):
 			if event not in self._events:
@@ -19,3 +31,8 @@ class Events(Library):
 		def emit(self, event, *args, **kwargs):
 			for handler in self._events.get(event, []):
 				handler(*args, **kwargs)
+
+	@staticmethod
+	def safe(handler, node):
+		# TODO
+		return handler
