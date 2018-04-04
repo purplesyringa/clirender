ddb60c2218be4f00f512065f0ae394b0a3fda532
Ivanq <imachug@yandex.ru>
2018-04-04 10:27:34 +0300

    Add .childNodes and .children to NodeAPI

--- clirender/layout/api.py
+++ clirender/layout/api.py
@@ -1,4 +1,4 @@
-from nodes import Node
+from nodes import Node, Generator
 
 class NodeAPI(object):
 	def getBoundaryBox(self):
@@ -7,6 +7,35 @@ class NodeAPI(object):
 			return node.getBoundaryBox()
 		else:
 			raise ValueError("getBoundaryBox(): Not defined on %s" % type(node))
+
+	@property
+	def childNodes(self):
+		node = storage[self]
+		if isinstance(node, Node):
+			return map(createAPI, node._children)
+		elif isinstance(node, Generator):
+			return map(createAPI, node.generate())
+		else:
+			raise ValueError("childNodes: Not defined on %s" % type(node))
+
+	@property
+	def children(self):
+		node = storage[self]
+		if isinstance(node, Node):
+			return map(createAPI, node.children)
+		elif isinstance(node, Generator):
+			return map(createAPI, self._get_children_recursively(node))
+		else:
+			raise ValueError("children: Not defined on %s" % type(node))
+
+	def _get_children_recursively(self, generator):
+		res = []
+		for node in generator.generate():
+			if isinstance(node, Generator):
+				res += self._get_children_recursively(node)
+			else:
+				res.append(node)
+		return res
 
 
 # storage[self] is a way to simulate private scope, so that Node or Generator
