4acc4f1d0eaaa9da2103a232446b4c54431335fd
Ivanq <imachug@yandex.ru>
2018-04-07 17:00:57 +0300

    Rename Char to Key

--- clirender/layout/libs/keypress.py
+++ clirender/layout/libs/keypress.py
@@ -2,7 +2,7 @@ from library import Library
 import os
 
 
-class Char(object):
+class Key(object):
 	def __init__(self, normal, shift=None, ctrl=None, padded=None, modifier=None):
 		self.normalCode = Padded(normal, padded)
 		self.shiftCode = Padded(shift, padded)
@@ -10,15 +10,15 @@ class Char(object):
 
 		self.modifier = modifier
 
-	def __eq__(self, char):
-		if not isinstance(char, Char):
-			raise ValueError("Not a char")
+	def __eq__(self, key):
+		if not isinstance(key, Key):
+			raise ValueError("Not a key")
 
 		return (
-			self.normalCode == char.normalCode and
-			self.shiftCode  == char.shiftCode  and
-			self.ctrlCode   == char.ctrlCode   and
-			self.modifier   is char.modifier
+			self.normalCode == key.normalCode and
+			self.shiftCode  == key.shiftCode  and
+			self.ctrlCode   == key.ctrlCode   and
+			self.modifier   is key.modifier
 		)
 
 class Padded(object):
@@ -37,91 +37,91 @@ class Padded(object):
 		return self.code == code.code and self.padded == code.padded
 
 
-def Ctrl(char):
-	if not isinstance(char, Char):
-		raise ValueError("Not a char")
-	return Char(char.normalCode, shift=char.shiftCode, ctrl=char.ctrlCode, modifier=Ctrl)
+def Ctrl(key):
+	if not isinstance(key, Key):
+		raise ValueError("Not a key")
+	return Key(key.normalCode, shift=key.shiftCode, ctrl=key.ctrlCode, modifier=Ctrl)
 
-def Shift(char):
-	if not isinstance(char, Char):
-		raise ValueError("Not a char")
-	return Char(char.normalCode, shift=char.shiftCode, ctrl=char.ctrlCode, modifier=Shift)
+def Shift(key):
+	if not isinstance(key, Key):
+		raise ValueError("Not a key")
+	return Key(key.normalCode, shift=key.shiftCode, ctrl=key.ctrlCode, modifier=Shift)
 
 
 # Via https://docs.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-6.0/aa299374(v=vs.60)
 class KeyCodes(object):
 	# Controls
-	Escape        = Char(27                                   )
-	Backspace     = Char(8,              ctrl=127             )
-	Tab           = Char(9,              ctrl=Padded(148, 0)  )
-	Enter         = Char(13,             ctrl=10              )
-	Space         = Char(32                                   )
-	Insert        = Char(82,             ctrl=146, padded=224 )
-	Delete        = Char(83,             ctrl=147, padded=224 )
+	Escape        = Key(27                                   )
+	Backspace     = Key(8,              ctrl=127             )
+	Tab           = Key(9,              ctrl=Padded(148, 0)  )
+	Enter         = Key(13,             ctrl=10              )
+	Space         = Key(32                                   )
+	Insert        = Key(82,             ctrl=146, padded=224 )
+	Delete        = Key(83,             ctrl=147, padded=224 )
 
 	# Digits
-	One           = Char(49,  shift=33                        )
-	Two           = Char(50,  shift=64                        )
-	Three         = Char(51,  shift=35                        )
-	Four          = Char(52,  shift=36                        )
-	Five          = Char(53,  shift=37                        )
-	Six           = Char(54,  shift=94                        )
-	Seven         = Char(55,  shift=38                        )
-	Eight         = Char(56,  shift=42                        )
-	Nine          = Char(57,  shift=40                        )
-	Zero          = Char(48,  shift=41                        )
-	Minus         = Char(45,  shift=95                        )
-	Plus          = Char(61,  shift=43                        )
+	One           = Key(49,  shift=33                        )
+	Two           = Key(50,  shift=64                        )
+	Three         = Key(51,  shift=35                        )
+	Four          = Key(52,  shift=36                        )
+	Five          = Key(53,  shift=37                        )
+	Six           = Key(54,  shift=94                        )
+	Seven         = Key(55,  shift=38                        )
+	Eight         = Key(56,  shift=42                        )
+	Nine          = Key(57,  shift=40                        )
+	Zero          = Key(48,  shift=41                        )
+	Minus         = Key(45,  shift=95                        )
+	Plus          = Key(61,  shift=43                        )
 
 	# Random characters
-	BracketLeft   = Char(91,  shift=123, ctrl=27              )
-	BracketRight  = Char(93,  shift=125, ctrl=29              )
-	Colon         = Char(59,  shift=58                        )
-	Quote         = Char(39,  shift=34                        )
-	Backtick      = Char(96,  shift=126                       )
-	Backslash     = Char(92,  shift=124, ctrl=28              )
-	Comma         = Char(44,  shift=60                        )
-	Dot           = Char(46,  shift=62                        )
-	Slash         = Char(47,  shift=63                        )
+	BracketLeft   = Key(91,  shift=123, ctrl=27              )
+	BracketRight  = Key(93,  shift=125, ctrl=29              )
+	Colon         = Key(59,  shift=58                        )
+	Quote         = Key(39,  shift=34                        )
+	Backtick      = Key(96,  shift=126                       )
+	Backslash     = Key(92,  shift=124, ctrl=28              )
+	Comma         = Key(44,  shift=60                        )
+	Dot           = Key(46,  shift=62                        )
+	Slash         = Key(47,  shift=63                        )
 
 	# Functional keys
-	F1            = Char(59,  shift=84,  ctrl=94,  padded=0   )
-	F2            = Char(60,  shift=85,  ctrl=95,  padded=0   )
-	F3            = Char(61,  shift=86,  ctrl=96,  padded=0   )
-	F4            = Char(62,  shift=87,  ctrl=97,  padded=0   )
-	F5            = Char(63,  shift=88,  ctrl=98,  padded=0   )
-	F6            = Char(64,  shift=89,  ctrl=99,  padded=0   )
-	F7            = Char(65,  shift=90,  ctrl=100, padded=0   )
-	F8            = Char(66,  shift=91,  ctrl=101, padded=0   )
-	F9            = Char(67,  shift=92,  ctrl=102, padded=0   )
-	F10           = Char(68,  shift=92,  ctrl=103, padded=0   )
-	F11           = Char(133, shift=135, ctrl=137, padded=224 )
-	F12           = Char(134, shift=136, ctrl=138, padded=224 )
+	F1            = Key(59,  shift=84,  ctrl=94,  padded=0   )
+	F2            = Key(60,  shift=85,  ctrl=95,  padded=0   )
+	F3            = Key(61,  shift=86,  ctrl=96,  padded=0   )
+	F4            = Key(62,  shift=87,  ctrl=97,  padded=0   )
+	F5            = Key(63,  shift=88,  ctrl=98,  padded=0   )
+	F6            = Key(64,  shift=89,  ctrl=99,  padded=0   )
+	F7            = Key(65,  shift=90,  ctrl=100, padded=0   )
+	F8            = Key(66,  shift=91,  ctrl=101, padded=0   )
+	F9            = Key(67,  shift=92,  ctrl=102, padded=0   )
+	F10           = Key(68,  shift=92,  ctrl=103, padded=0   )
+	F11           = Key(133, shift=135, ctrl=137, padded=224 )
+	F12           = Key(134, shift=136, ctrl=138, padded=224 )
 
 	# Navigation
-	Home          = Char(71,             ctrl=119, padded=224 )
-	ArrowUp       = Char(72,             ctrl=141, padded=224 )
-	PageUp        = Char(73,             ctrl=134, padded=224 )
-	ArrowLeft     = Char(75,             ctrl=115, padded=224 )
-	ArrowRight    = Char(77,             ctrl=116, padded=224 )
-	End           = Char(79,             ctrl=117, padded=224 )
-	Down          = Char(80,             ctrl=145, padded=224 )
-	PageDown      = Char(81,             ctrl=118, padded=224 )
+	Home          = Key(71,             ctrl=119, padded=224 )
+	ArrowUp       = Key(72,             ctrl=141, padded=224 )
+	PageUp        = Key(73,             ctrl=134, padded=224 )
+	ArrowLeft     = Key(75,             ctrl=115, padded=224 )
+	ArrowRight    = Key(77,             ctrl=116, padded=224 )
+	End           = Key(79,             ctrl=117, padded=224 )
+	Down          = Key(80,             ctrl=145, padded=224 )
+	PageDown      = Key(81,             ctrl=118, padded=224 )
 
 
 	@classmethod
-	def fromChar(cls, key):
+	def fromChar(cls, char):
 		for name in cls.__dict__:
-			char = cls.__dict__[name]
-			if not isinstance(char, Char):
+			key = cls.__dict__[name]
+			if not isinstance(key, Key):
 				continue
 
-			if key == char.normalCode:
-				return char
-			elif key == char.shiftCode:
-				return Shift(char)
-			elif key == char.ctrlCode:
-				return Ctrl(char)
+			if char == key.normalCode:
+				return key
+			elif char == key.shiftCode:
+				return Shift(key)
+			elif char == key.ctrlCode:
+				return Ctrl(key)
 
 		return None
 
@@ -135,7 +135,7 @@ for letter in range(ord("a"), ord("z") + 1):
 		# ^H (Backspace), ^I (Tab), ^J (Ctrl+Enter) or ^M(Enter)
 		ctrl = None
 
-	setattr(KeyCodes, chr(shift), Char(normal, shift=shift, ctrl=ctrl))
+	setattr(KeyCodes, chr(shift), Key(normal, shift=shift, ctrl=ctrl))
 
 
 def waitKey():
