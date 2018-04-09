fa0f0d6b3bcb01e1eafcf347535e75c6bdfc9165
Ivanq <imachug@yandex.ru>
2018-04-09 18:39:34 +0300

    Add byDefineName() method

--- clirender/layout/libs/query.py
+++ clirender/layout/libs/query.py
@@ -10,20 +10,37 @@ class Query(Library):
 		def __init__(self, *args, **kwargs):
 			patch.super(Query.Layout, self).__init__(*args, **kwargs)
 			self._byTagName = {}
+			self._byDefineName = {}
 
 	@patch(Element)
 	class Element(object):
 		def init(self, **kwargs):
+			# Normal tags
 			tag = type(self).__name__
 			if tag not in self.layout._byTagName:
 				self.layout._byTagName[tag] = []
 			self.layout._byTagName[tag].append(self)
 
+			# <Define>
+			if id(self.elem) == self.slots.get("__root__", None):
+				name = self.slots["__name__"]
+				if name not in self.layout._byDefineName:
+					self.layout._byDefineName[name] = []
+				self.layout._byDefineName[name].append(self)
+
+
 			patch.super(Query.Element, self).init(**kwargs)
 
 		def destroy(self):
+			# Normal tags
 			tag = type(self).__name__
 			self.layout._byTagName[tag].remove(self)
+
+			# <Define>
+			if id(self.elem) == self.slots.get("__root__", None):
+				name = self.slots["__name__"]
+				self.layout._byDefineName[name].remove(self)
+
 
 			patch.super(Query.Element, self).destroy()
 
@@ -31,3 +48,6 @@ class Query(Library):
 	class LayoutAPI(object):
 		def byTagName(self, name):
 			return [createAPI(node) for node in self._byTagName.get(name, [])]
+
+		def byDefineName(self, name):
+			return [createAPI(node) for node in self._byDefineName.get(name, [])]
