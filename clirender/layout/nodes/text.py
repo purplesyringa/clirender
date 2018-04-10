56248c7e08d755bb41d1b13d31419c879bf99e44
Ivanq <imachug@yandex.ru>
2018-04-10 13:16:41 +0300

    Handle top and bottom boundaries

--- clirender/layout/nodes/text.py
+++ clirender/layout/nodes/text.py
@@ -33,19 +33,31 @@ class Text(Node):
 
 
 		text = self.value[:int(width)]
+		cwidth, cheight = width, height
 
 		# Left boundary
 		text = text[int(max(self.render_boundary_left_top[0] - x, 0)):]
 		x = max(x, self.render_boundary_left_top[0])
 
 		# Right boundary
-		pos = max(x + width - self.render_boundary_right_bottom[0], 0)
+		pos = max(x + cwidth - self.render_boundary_right_bottom[0], 0)
 		if pos > 0:
 			text = text[:-int(pos)]
-		width = min(width, self.render_boundary_right_bottom[0] - x)
+		cwidth = min(cwidth, self.render_boundary_right_bottom[0] - x)
+
+		# Top boundary
+		if self.fill:
+			y = max(y, self.render_boundary_left_top[1])
+
+		# Bottom boundary
+		cheight = min(cheight, self.render_boundary_right_bottom[1] - y)
 
 
-		if not (self.render_boundary_left_top[1] <= y <= y + height <= self.render_boundary_right_bottom[1]):
+		if x < self.render_boundary_left_top[0] or x >= self.render_boundary_right_bottom[0]:
+			return width, height
+		elif y < self.render_boundary_left_top[1] or y >= self.render_boundary_right_bottom[1]:
+			return width, height
+		elif cwidth == 0 or cheight == 0:
 			return width, height
 
 		bg = self.inherit("bg")
@@ -55,9 +67,9 @@ class Text(Node):
 		if color is not None:
 			if not dry_run:
 				if self.fill:
-					self.layout.screen.fill(x, y, x + width, y + height, char=self.value, style=lambda s: self.layout.screen.colorize(s, bg=bg, fg=color, bright=bright))
+					self.layout.screen.fill(x, y, x + cwidth, y + cheight, char=self.value, style=lambda s: self.layout.screen.colorize(s, bg=bg, fg=color, bright=bright))
 				else:
 					self.layout.screen.printAt(self.layout.screen.colorize(text, bg=bg, fg=color, bright=self.bright), x, y)
-					self.layout.screen.printAt(self.layout.screen.colorize(" " * int(width - len(text)), bg=bg), x + len(text), y)
+					self.layout.screen.printAt(self.layout.screen.colorize(" " * int(cwidth - len(text)), bg=bg), x + len(text), y)
 
 		return width, height
